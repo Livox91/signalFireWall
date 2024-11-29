edd29761bed374dc25f912f2ee1341363d0d9b3d
bilaladnan768 <231581@students.au.edu.pk>
2024-11-29 22:58:56 +0500

    Im Stupid

--- src/headers/signal.h
+++ src/headers/signal.h
@@ -3,19 +3,21 @@
 
 
 class Signal{
-    Signal* prev;
-    Signal* next;
     bool State;
     int SignalId;
     //x-cord
     //y-cord
-    sf::Texture SignalTexture;
+    sf::Texture SignalTextureRed;
+    sf::Texture SignalTextureYellow;
+    sf::Texture SignalTextureGreen;
     sf::Sprite SignalSprite;
     
     public:
+    Signal* prev;
+    Signal* next;
     Signal(){
-        SignalTexture=NULL;
-        SignalSprite.setTexture(SignalTexture);
+        //SignalTexture=NULL;
+        SignalSprite.setTexture(SignalTextureRed);
         prev=NULL;
         next=NULL;
         State=false;
@@ -23,18 +25,47 @@ class Signal{
     }//constructor
     Signal(int id){
         SignalId=id;
-    }
+        if(SignalId=1){
+            State=true;
+            SignalSprite.setTexture(SignalTextureGreen);
+        }
+        else{
+         SignalSprite.setTexture(SignalTextureRed);
+        }
+
+    }//parameterized constructor
 
     void changeState(){
-            State = !State;  
+            State = !State; 
+            if(SignalSprite.getTexture()==SignalTextureRed){
+                SignalSprite.setTexture(SignalTextureYellow);
+                SingalSprite.setTexture(SignalTextureGreen);
+            } 
+            else{
+             SingalSprite.setTexture(SignalTextureRed);
+            }
     }//changeState
 
     void setSpritePosition(float a,float b){
         SignalSprite.setPosition(x,y);
-    }//setPosition
+    }//setSpritePosition
 
-    void spriteRotation(float a){
+    void setSpriteRotation(float a){
         SignalSprite.setRotation(a);
-    }//spriteRotation
+    }//setSpriteRotation
+
+    int getSignalId(){
+        return SignalId;
+    }//getSignalId
+
+    void onPriority(){
+        State=true;
+        SingalSprite.setTexture(SignalTextureGreen);
+    }//onPriority
+
+    void onOff(){
+        State=false;
+        SignalSprite.setTexture(SignalTextureRed);
+    }//onOff
 
 }
